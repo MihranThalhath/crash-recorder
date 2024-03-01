c8cd39aedbabe4501161afccbc43ea7894e71b8b
Mihran Thalhath <mihranz7@gmail.com>
2024-03-02 00:01:28 +0530

    feat: improvements
    - provide the rrweb files as part of module
    - run black for python
    - add missing author

--- crash_recorder/__manifest__.py
+++ crash_recorder/__manifest__.py
@@ -1,27 +1,24 @@
-# coding: utf-8
 # Copyright (c) 2021, Joren Van Onder <joren@jvo.sh>
-# 
+#
 # This file is part of crash-recorder.
-# 
+#
 # crash-recorder is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # crash-recorder is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with crash-recorder.  If not, see <https://www.gnu.org/licenses/>.
 {
     "name": "Crash Recorder",
     "category": "Tools",
-    "description": """
-Automatically record user actions leading to errors.
-""",
-    "version": "1.0",
+    "summary": "Automatically record user actions leading to errors.",
+    "version": "14.0.1.0.1",
     "depends": ["web"],
     "data": [
         "views/rrweb_recording_views.xml",
@@ -32,5 +29,6 @@ Automatically record user actions leading to errors.
         "static/src/xml/rrweb_field.xml",
         "static/src/xml/crash_recorder_systray.xml",
     ],
+    "author": "Joren Van Onder",
     "license": "LGPL-3",
 }
--- crash_recorder/models/rrweb_recording.py
+++ crash_recorder/models/rrweb_recording.py
@@ -1,24 +1,25 @@
-# coding: utf-8
 # Copyright (c) 2021, Joren Van Onder <joren@jvo.sh>
-# 
+#
 # This file is part of crash-recorder.
-# 
+#
 # crash-recorder is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # crash-recorder is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with crash-recorder.  If not, see <https://www.gnu.org/licenses/>.
-from odoo import api, fields, models, SUPERUSER_ID, _
+
 from base64 import b64encode, b64decode
 import logging
 import zlib
+
+from odoo import api, fields, models
 
 _logger = logging.getLogger(__name__)
 
@@ -45,13 +46,13 @@ class RRWebRecording(models.Model):
             )
 
     def _inverse_events(self):
-        """ A recording can be >3 MiB, so compress it. Compression ratio is ~10. """
+        """A recording can be >3 MiB, so compress it. Compression ratio is ~10."""
         for recording in self:
             recording.events_compressed = b64encode(zlib.compress(recording.events))
 
     @api.autovacuum
     def _gc_recordings(self):
-        """ Avoid bloating the database with lots of recordings. """
+        """Avoid bloating the database with lots of recordings."""
         limit_date = fields.Datetime.subtract(fields.Datetime.now(), days=30)
         to_unlink = self.search([("create_date", "<", limit_date)])
         _logger.info(
